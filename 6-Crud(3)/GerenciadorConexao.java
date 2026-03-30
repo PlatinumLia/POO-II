ff616ae289d8587b46981f9d8b7de7a036a2e4e3
Lia <134401063+PlatinumLia@users.noreply.github.com>
2026-03-30 20:24:57 -0300

    Add files via upload

---
+++ 6-Crud(3)/GerenciadorConexao.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class GerenciadorConexao{
+    private static Connection conexao;
+
+    public static Connection pegarConexao() throws SQLException{
+        String url = "jdbc:mysql://127.0.0.1/db_locadora";
+        String user = "root";
+        String senha = "bancodedados";
+    
+        conexao = DriverManager.getConnection(url, user, senha);
+
+        return conexao;
+    }
+}
